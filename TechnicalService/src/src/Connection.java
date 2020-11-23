fbd29bd12b1f4f734d17619bfeeb4c8c0aee9444
ClawGibson <44484064+ClawGibson@users.noreply.github.com>
2020-11-22 22:45:48 -0600

    Connection

--- TechnicalService/src/src/Connection.java
+++ TechnicalService/src/src/Connection.java
@@ -1,5 +1,6 @@
 package src;
 
+import jpl.Query;
 
 /**
  *
@@ -8,6 +9,10 @@ package src;
 public class Connection {
 
     public static void main(String[] args) {
-        
+        // Consulting file.
+        String t1 = "consult(‘archivo.pl’)";
+        Query q1 = new Query(t1);
+        // Message.
+        System.out.println(t1 + " " + (q1.hasSolution() ? "true" : "false"));
     }
 }
