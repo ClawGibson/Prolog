48709e6169f3ed8b8d8b165174c2450f99d7e2d1
ClawGibson <44484064+ClawGibson@users.noreply.github.com>
2020-11-25 10:39:32 -0600

    New version

--- TechnicalService/src/src/Connection.java
+++ TechnicalService/src/src/Connection.java
@@ -10,7 +10,7 @@ public class Connection {
 
     public static void main(String[] args) {
         // Consulting file.
-        String t1 = "consult(‘archivo.pl’)";
+        String t1 = "consult(‘ExpertSystem.pl’)";
         Query q1 = new Query(t1);
         // Message.
         System.out.println(t1 + " " + (q1.hasSolution() ? "true" : "false"));
