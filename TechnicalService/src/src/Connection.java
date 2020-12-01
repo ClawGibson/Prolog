3681d5befbac5ecbac5f1722f728b374d03d7616
Gamaliel Bernal <gamalielbj@gmail.com>
2020-12-01 17:25:28 -0600

    Connection notes.
    Added a description of the connection.

--- TechnicalService/src/src/Connection.java
+++ TechnicalService/src/src/Connection.java
@@ -12,12 +12,25 @@ public class Connection {
 
     }
 
-    public String Conn() {
-        // Consulting file.
-        String path = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/expertsystem.pl')";
-        Query q1 = new Query(path);
+    public static String Conn() {
+
+        /**
+         * Debe de consultarse la base de conocimiento primero para que el
+         * sistema experto la conozca y pueda realizar las consultas.
+         */
+        // Consulting files.
+        String path1 = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/expertsystem.pl')";
+        String path2 = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/knowledgebase.pl')";
+        String result;
+
+        Query q2 = new Query(path2), q1 = new Query(path1);
         // Message.
-        //System.out.println("consult:" + " " + (q1.hasSolution() ? "true" : "false"));
-        return "consult ExpertSystem: " + " " + (q1.hasSolution() ? "true" : "false");
+        if (q1.hasSolution() && q2.hasSolution()) {
+            result = "consult KnowledgeBase: " + " " + (q2.hasSolution() ? "true" : "false")
+                    + "\nconsult ExpertSystem: " + " " + (q1.hasSolution() ? "true" : "false");
+            return result;
+        } else {
+            return "Error en la conexión";
+        }
     }
 }
