d291674ebd819752d9ea3ee7a53cca2cca320842
Gamaliel Bernal <gamalielbj@gmail.com>
2020-12-01 13:11:43 -0600

    New Interface
    Added checkboxes.

--- TechnicalService/src/src/Connection.java
+++ TechnicalService/src/src/Connection.java
@@ -8,11 +8,16 @@ import org.jpl7.Query;
  */
 public class Connection {
 
-    public static void main(String[] args) {
+    public void Connection() {
+
+    }
+
+    public String Conn() {
         // Consulting file.
         String path = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/expertsystem.pl')";
         Query q1 = new Query(path);
         // Message.
-        System.out.println("consult:" + " " + (q1.hasSolution() ? "true" : "false"));
+        //System.out.println("consult:" + " " + (q1.hasSolution() ? "true" : "false"));
+        return "consult ExpertSystem: " + " " + (q1.hasSolution() ? "true" : "false");
     }
 }
