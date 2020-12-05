1fc2dc0d7c5bed3b84e67ae1cb69bc9aa5550989
Gamaliel Bernal <gamalielbj@gmail.com>
2020-12-04 19:27:19 -0600

    Try #1
    - Modified text of check box.
    - Added 1 check box.
    - First attempt to call "consulta." query in PROLOG.

--- TechnicalService/src/src/Connection.java
+++ TechnicalService/src/src/Connection.java
@@ -1,6 +1,8 @@
 package src;
 
+import java.util.Map;
 import org.jpl7.Query;
+import org.jpl7.Term;
 
 /**
  *
@@ -8,16 +10,39 @@ import org.jpl7.Query;
  */
 public class Connection {
 
+    public static void main(String args[]) {
+        System.out.println(Conn());
+        Results();
+    }
+
     public void Connection() {
 
     }
 
-    public String Conn() {
+    public static String Conn() {
         // Consulting file.
         String path = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/expertsystem.pl')";
+        String path2 = "consult('C:/Users/GamalielJakousi/Documents/GitHub/Prolog/TechnicalService/src/src/knowledgebase.pl')";
         Query q1 = new Query(path);
+        Query q2 = new Query(path2);
         // Message.
-        //System.out.println("consult:" + " " + (q1.hasSolution() ? "true" : "false"));
-        return "consult ExpertSystem: " + " " + (q1.hasSolution() ? "true" : "false");
+        return "consult KnowlegdeBase: " + " " + (q2.hasSolution() ? "true" : "false")
+                + "\nconsult ExpertSystem: " + " " + (q1.hasSolution() ? "true" : "false");
+    }
+
+    public static void Results() {
+        Query query = new Query("consulta.");
+
+        while (query.hasMoreSolutions()) {
+            Map<String, Term> solution = query.nextSolution();
+            System.out.println("uno");
+        }
+
+//        java.util.Map<String, Term>[] solutions = query.allSolutions();
+//        for (int i = 0; i < solutions.length; i++) {
+//            System.out.println("X = " + solutions[i].get("X"));
+//        }
+        //java.util.Map<String, Term> solution;
+        //solution = query.oneSolution();
     }
 }
